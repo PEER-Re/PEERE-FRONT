57ffa6e07529fe4a0b218d68b33f95eab856709f
eunji <bian87@dgu.ac.kr>
2024-02-16 06:04:22 +0900

    FIX: 카카오 로그인 원복

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import axios from 'axios';
+import a
 import { HomeContainer, LogoImg, MotoImg, LoginButton, HomeBg, HomeBox } from '/src/styles/style';
 
 const Home = () => {
 
     // kakao login 요청
-   /* const handleLogin = () => {
+    const handleLogin = () => {
         window.location.href = `${
             import.meta.env.VITE_APP_SERVER_HOST
           }/oauth2/authorization/kakao`;
-    }; */
-
-    const handleLogin = async () => {
-        try {
-          const response = await axios.get(`${
-            import.meta.env.VITE_APP_SERVER_HOST
-          }/oauth2/authorization/kakao`);
-          
-          console.log(response.data);
-
-        } catch(error) {
-          console.log(error);
-        }
-    }
+    };
 
     return (
             <HomeContainer>
