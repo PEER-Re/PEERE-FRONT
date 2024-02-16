ff5fc1a8e484cc1e05e9f3a715dcf2d0400d822e
eunji <bian87@dgu.ac.kr>
2024-02-16 10:14:28 +0900

    FIX: api 수정

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,35 +1,23 @@
 import { HomeContainer, LogoImg, MotoImg, LoginButton, HomeBg, HomeBox } from '/src/styles/style';
-import { useEffect, useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import axios from 'axios';
 
 const Home = () => {
 
-    const { search } = useLocation();
-    console.log('search : ', search);
-    const code = new URL(window.location.href).searchParams.get("code");
-    console.log('code : ', code);
-  
-    useEffect(() => {
-      console.log('경로 : 시작');
-  
-      const confirmLogin = async () => {
-        const params = new URLSearchParams(search);
-        const code = params.get("code");
-        if (code) {
-          console.log('code : ', code);
-          return code;
-        }
-      };
-  
-      const code = confirmLogin();
-  }, []);
-
-    // kakao login 요청
+   /* // kakao login 요청
     const handleLogin = () => {
         window.location.href = `${
             import.meta.env.VITE_APP_SERVER_HOST
           }/oauth2/authorization/kakao`;
-    };
+    }; */
+    const handleLogin = () => {
+      axios.get(`${import.meta.env.VITE_APP_SERVER_HOST}/oauth2/authorization/kakao`)
+      .then(response => {
+          console.log(response.data);
+      })
+      .catch(error => {
+          console.error(error);
+      });
+  };
 
     return (
             <HomeContainer>
