b4ff132836900a260428b889290bcfd661f59392
yejin <yj040102@naver.com>
2024-02-18 11:01:49 +0900

    Fix: home 오류해결

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import { HomeContainer, HomeBox, LogoImg, MotoImg, HomeLogo } from '/src/styles/style';
 
 const Home = () => {
-    const Rest_api_key = process.env.REACT_APP_KAKAO_REST_API_KEY; 
-    const redirect_uri = 'http://localhost:8080/oauth2/authorization/kakao';
-    // oauth 요청 URL
-    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
-    const handleLogin = () => {
-        window.location.href = kakaoURL;
-    };
+
 
     return (
         <>
